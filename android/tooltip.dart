c23fae111d617888c05b97ed5198038cb8787ade
Talat Mahmud Oyshik <58624651+moyshik7@users.noreply.github.com>
2020-09-14 12:52:58 +0600

    Create tooltip.dart

---
+++ android/tooltip.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+import 'package:gallery/l10n/gallery_localizations.dart';
+
+class TooltipDemo extends StatelessWidget {
+
+    const TooltipDemo();
+
+    @override
+
+    Widget build(BuildContext context) {
+
+        return Scaffold(
+
+            appBar: AppBar(
+
+                automaticallyImplyLeading: false,
+
+                title: Text(GalleryLocalizations.of(context).demoTooltipTitle),
+
+            ),
+
+            body: Center(
+
+                child: Padding(
+
+                    padding: const EdgeInsets.all(8),
+
+                    child: Column(
+
+                        mainAxisAlignment: MainAxisAlignment.center,
+
+                        children: [
+
+                            Text(
+
+                                GalleryLocalizations.of(context).demoTooltipInstructions,
+
+                                textAlign: TextAlign.center,
+
+                            ),
+
+                            const SizedBox(height: 16),
+
+                            Tooltip(
+
+                                message:
+
+                                        GalleryLocalizations.of(context).starterAppTooltipSearch,
+
+                                child: IconButton(
+
+                                    color: Theme.of(context).colorScheme.primary,
+
+                                    onPressed: () {},
+
+                                    icon: const Icon(Icons.search),
+
+                                ),
+
+                            ),
+
+                        ],
+
+                    ),
+
+                ),
+
+            ),
+
+        );
+
+    }
+
+}
